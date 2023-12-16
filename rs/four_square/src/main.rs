549592377ea00431a6ac3f767a6d8c40b92e7aca
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2023-12-16 18:32:29 +0000

    simulated annealing
    works pretty consistently with very high threshold for unchanged iterations

--- rs/four_square/src/main.rs
+++ rs/four_square/src/main.rs
@@ -1,3 +1,7 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use rand::prelude::*;
 use rand::rngs::mock::StepRng;
 use shuffle::irs::Irs;
 use shuffle::shuffler::Shuffler;
@@ -6,14 +10,15 @@ use std::collections::HashSet;
 use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::iter::FromIterator;
 
-#[allow(dead_code)]
+#[derive(Copy, Clone)]
 
 struct Key {
-    a: [char; 25],
-    b: [char; 25],
-    c: [char; 25],
-    d: [char; 25],
+    a: [u8; 25],
+    b: [u8; 25],
+    c: [u8; 25],
+    d: [u8; 25],
 }
 
 struct Coords {
@@ -21,26 +26,23 @@ struct Coords {
     column: i32,
 }
 
-fn letters() -> [char; 25] {
-    let l: [char; 25] = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-        'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    ];
-    l
-}
+const LETTERS: [u8; 25] = [
+    65, 66, 67, 68, 69, 70, 71, 72, 73, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
+    90,
+];
 
-fn format_text(original: String) -> String {
-    let mut result: String = "".to_string();
+fn format_text(original: Vec<u8>) -> Vec<u8> {
+    let mut result: Vec<u8> = Vec::new();
     for i in 0..original.len() {
-        if original.as_bytes()[i] >= 65 && original.as_bytes()[i] <= 90 {
-            result.push(original.as_bytes()[i] as char)
+        if original[i] >= 65 && original[i] <= 90 {
+            result.push(original[i])
         }
     }
     result
 }
 
-fn scan_tetragrams() -> HashMap<String, i32> {
-    let mut tg: HashMap<String, i32> = HashMap::new();
+fn scan_tetragrams() -> HashMap<Vec<u8>, i32> {
+    let mut tg: HashMap<Vec<u8>, i32> = HashMap::new();
     let file = File::open("../tetragrams.txt").expect("Failed to load tetragram file");
     let reader = BufReader::new(&file);
     for line in reader.lines() {
@@ -49,97 +51,93 @@ fn scan_tetragrams() -> HashMap<String, i32> {
         let n: Result<i32, _> = fields[1].parse();
         let num: i32 = match n {
             Ok(n) => n,
-            Err(_) => panic!("Failed to convert tetragram frequence to i32"),
+            Err(_) => panic!("Failed to convert tetragram frequency to i32"),
         };
-        tg.insert(fields[0].to_string(), num);
+        tg.insert(fields[0].as_bytes().to_vec(), num);
     }
     tg
 }
 
-fn gen_reference(n: usize) -> String {
-    let r: Vec<u8> = format_text(read("../referenceText.txt"))
-        .as_bytes()
-        .to_vec();
-    let mut ref_text: String = String::from("");
+fn gen_reference(n: usize) -> Vec<u8> {
+    let r: Vec<u8> = format_text(read("../referenceText.txt"));
+    let mut ref_text: Vec<u8> = Vec::new();
     for i in 0..n {
-        ref_text = format!("{}{}", ref_text, String::from(r[i] as char));
+        ref_text.push(r[i]);
     }
     ref_text
 }
 
-fn scan_dict() -> Vec<String> {
-    let words: Vec<String> = read("../challenge_word_list.txt")
+fn scan_dict() -> Vec<Vec<u8>> {
+    //very inefficient with conversions, but this is only called once
+    let words: Vec<String> = String::from_utf8(read("../challenge_word_list.txt").clone())
+        .unwrap()
         .split(" ")
         .collect::<Vec<&str>>()
         .iter()
         .map(|&s| s.into())
         .collect(); //this was painful lmao
-    words[..words.len() - 1].to_vec() //last element is a newline character otherwise
+    let mut dict: Vec<Vec<u8>> = Vec::new();
+    for mut w in words {
+        w.retain(|c| {
+            let mut seen: HashSet<char> = HashSet::new();
+            let s = seen.contains(&c);
+            seen.insert(c);
+            !s
+        });
+        if w.contains("J") || w.len() == 1 {
+            continue;
+        }
+        dict.push(w.into_bytes())
+    }
+    dict[..dict.len() - 1].to_vec() //last value is a newline character
 }
 
-fn randomise() -> [char; 25] {
-    let mut g: Vec<char> = letters().to_vec();
+fn randomise() -> [u8; 25] {
+    let mut g: Vec<u8> = LETTERS.to_vec();
     let mut rng = StepRng::new(0, 1);
     let mut irs = Irs::default();
     let _ = irs.shuffle(&mut g, &mut rng);
-    let t: Result<[char; 25], _> = g.try_into();
+    let t: Result<[u8; 25], _> = g.try_into();
     match t {
         Ok(arr) => arr,
         Err(_) => panic!("Failed to randomise grid"),
     }
 }
 
-fn read(path: &str) -> String {
-    let f: Result<String, std::io::Error> = fs::read_to_string(path);
+fn read(path: &str) -> Vec<u8> {
+    let f: Result<Vec<u8>, std::io::Error> = fs::read(path);
     match f {
         Ok(s) => s,
         Err(_) => panic!("Couldn't find file"),
     }
 }
 
-fn score(text: &String, tetragrams: &HashMap<String, i32>) -> i32 {
-    let bytes = text.as_bytes();
+fn score(text: &Vec<u8>, tetragrams: &HashMap<Vec<u8>, i32>) -> i32 {
     let mut total: i32 = 0;
     for i in 0..text.len() - 3 {
-        let v: Vec<u8> = (bytes[i..i + 4]).to_vec();
-        let s: Result<String, _> = String::from_utf8(v);
-        let slice = match s {
-            Ok(s) => s,
-            Err(_) => panic!("Failed to parse window in text"),
-        };
-        if tetragrams.contains_key(&slice) {
-            total += tetragrams[&slice];
+        let v: Vec<u8> = text[i..i + 4].to_vec();
+        if tetragrams.contains_key(&v) {
+            total += tetragrams[&v];
         }
     }
     total
 }
 
-fn fill_grid(keyword: &mut String) -> [char; 25] {
-    let mut seen: HashSet<char> = HashSet::new();
-    keyword.retain(|c| {
-        let s = seen.contains(&c);
-        seen.insert(c);
-        !s
-    }); //remove duplicate letters from keyword
-
-    let last_idx = letters()
+fn fill_grid(keyword: Vec<u8>) -> [u8; 25] {
+    let mut seen: HashSet<u8> = HashSet::from_iter(keyword.clone()); //collect letters from keyword
+    let last_idx = LETTERS
         .iter()
-        .position(|&x| x == keyword.chars().last().unwrap())
+        .position(|&x| x == *keyword.last().unwrap())
         .unwrap(); //continues filling up grid with next letter after last letter in keyword
-    let mut grid: Vec<char> = keyword
-        .as_bytes()
-        .to_vec()
-        .iter()
-        .map(|x| *x as char)
-        .collect(); //also painful
+    let mut grid: Vec<u8> = keyword; //fill with letters of keyword
     for i in last_idx..last_idx + 25 {
         let idx = i % 25;
-        if !seen.contains(&letters()[idx]) {
-            seen.insert(letters()[idx]);
-            grid.push(letters()[idx]);
+        if !seen.contains(&LETTERS[idx]) {
+            seen.insert(LETTERS[idx]);
+            grid.push(LETTERS[idx]);
         }
     }
-    let t: Result<[char; 25], _> = grid.try_into();
+    let t: Result<[u8; 25], _> = grid.try_into();
     match t {
         Ok(arr) => arr,
         Err(_) => panic!("Failed to fill grid"),
@@ -156,13 +154,12 @@ fn grid_index(row: i32, column: i32) -> i32 {
     row * 5 + column
 }
 
-fn decipher(text: &String, k: Key) -> Option<String> {
-    let mut deciphered: String = String::from("");
-    let bytes = text.as_bytes();
+fn decipher(text: &Vec<u8>, k: Key) -> Option<Vec<u8>> {
+    let mut deciphered: Vec<u8> = Vec::new();
     for i in (0..text.len() - 1).step_by(2) {
-        let v: Vec<u8> = (bytes[i..i + 2]).to_vec();
-        let i1: usize = k.b.iter().position(|&x| x == v[0] as char)?;
-        let i2: usize = k.c.iter().position(|&x| x == v[1] as char)?;
+        let v: Vec<u8> = (text[i..i + 2]).to_vec();
+        let i1: usize = k.b.iter().position(|&x| x == v[0])?;
+        let i2: usize = k.c.iter().position(|&x| x == v[1])?;
         let coords1: Coords = coordinates(i1.try_into().ok()?);
         let coords2: Coords = coordinates(i2.try_into().ok()?);
 
@@ -173,44 +170,105 @@ fn decipher(text: &String, k: Key) -> Option<String> {
             .try_into()
             .expect("Failed to get grid index 2");
 
-        let letter1: String = String::from(k.a[d_i1]);
-        let letter2: String = String::from(k.d[d_i2]);
-        deciphered = format!("{}{}{}", deciphered, letter1, letter2);
+        deciphered.push(k.a[d_i1]);
+        deciphered.push(k.d[d_i2]);
     }
     Some(deciphered)
 }
 
-fn dictionary_attack(ciphertext: &String) {
+fn dictionary_attack(ciphertext: &Vec<u8>) {
     let dict = scan_dict();
     let tetragrams = scan_tetragrams();
     let ref_score = score(&gen_reference(ciphertext.clone().len()), &tetragrams);
-    let mut count: i32 = 0;
     for word in &dict {
-        println!("{}", count);
-        count += 1;
-        if word.contains("J") || word.len() == 1 {
-            continue;
-        }
         for word2 in &dict {
-            if word2.contains("J") || word2.len() == 1 {
-                continue;
-            }
             let k: Key = Key {
-                a: letters(),
-                b: fill_grid(&mut word.to_string()),
-                c: fill_grid(&mut word2.to_string()),
-                d: letters(),
+                a: LETTERS,
+                b: fill_grid(word.to_vec()),
+                c: fill_grid(word2.to_vec()),
+                d: LETTERS,
             };
-            let p = decipher(&ciphertext, k).unwrap();
+            let p = decipher(ciphertext, k).unwrap();
             if score(&p, &tetragrams) * 10 >= ref_score * 8 {
-                println!("{}", p)
+                println!("{}", String::from_utf8(p).unwrap());
             }
         }
     }
 }
 
+fn mutate(k: &mut Key) {
+    let mut rng = rand::thread_rng();
+    let x: f64 = rng.gen(); //rng choosing which grid to mutate
+    if x > 0.5 {
+        let i1 = rng.gen_range(0..25);
+        let mut i2 = rng.gen_range(0..25);
+        while i1 == i2 {
+            i2 = rng.gen_range(0..25);
+        }
+        let temp = k.b[i1];
+        k.b[i1] = k.b[i2];
+        k.b[i2] = temp
+    } else {
+        let i1 = rng.gen_range(0..25);
+        let mut i2 = rng.gen_range(0..25);
+        while i1 == i2 {
+            i2 = rng.gen_range(0..25);
+        }
+        let temp = k.c[i1];
+        k.c[i1] = k.c[i2];
+        k.c[i2] = temp
+    }
+}
+
+fn acceptance_probability(delta_e: f64, temp: f64) -> f64 {
+    delta_e * temp
+}
+
+fn simulated_annealing(ciphertext: &Vec<u8>, max_constant: i32, max_temp: f64, k: f64) {
+    let mut iterations_count = 0;
+    let mut temp = max_temp;
+    let tetragrams: HashMap<Vec<u8>, i32> = scan_tetragrams();
+    let mut new_key = Key {
+        a: LETTERS,
+        b: randomise(),
+        c: randomise(),
+        d: LETTERS,
+    };
+    let mut constant: i32 = 0;
+    while constant < max_constant {
+        iterations_count += 1;
+        let old_key = new_key.clone();
+        mutate(&mut new_key);
+        let s_old: f64 = 1.0;
+        let s_new: f64 = score(&decipher(ciphertext, new_key).unwrap(), &tetragrams) as f64
+            / score(&decipher(ciphertext, old_key).unwrap(), &tetragrams) as f64;
+        let delta_e: f64 = s_new / s_old;
+        if delta_e < 1.0 {
+            //old key is better
+            let mut rng = rand::thread_rng();
+            let x: f64 = rng.gen();
+            let p = acceptance_probability(delta_e, temp);
+            if x < p {
+                constant = 0;
+            } else {
+                new_key = old_key;
+                constant += 1;
+            }
+        } else {
+            constant = 0;
+        }
+        temp *= k;
+    }
+    println!(
+        "{}",
+        String::from_utf8(decipher(ciphertext, new_key).unwrap()).unwrap()
+    );
+    println!("{} iterations", iterations_count)
+}
+
 fn main() {
     let original = read("src/ciphertext.txt");
-    let ciphertext: String = format_text(original);
-    dictionary_attack(&ciphertext)
+    let ciphertext: Vec<u8> = format_text(original);
+    simulated_annealing(&ciphertext, 1000, 1.0, 0.95);
+    //dictionary_attack(&ciphertext);
 }
